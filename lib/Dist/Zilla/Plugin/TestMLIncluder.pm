04094673c358a0c2dbb277bd977f241c8a632e60
Ingy döt Net <ingy@ingy.net>
2014-05-24 06:13:32 -0700

    v0.02

--- lib/Dist/Zilla/Plugin/TestMLIncluder.pm
+++ lib/Dist/Zilla/Plugin/TestMLIncluder.pm
@@ -1,8 +1,8 @@
+# ABSTRACT: Ship your TestML version
+
 package Dist::Zilla::Plugin::TestMLIncluder;
 
 use Moose;
-
-# ABSTRACT: Ship your TestML version
 
 extends 'Dist::Zilla::Plugin::ModuleIncluder';
 
@@ -70,35 +70,3 @@ __PACKAGE__->meta->make_immutable;
 no Moose;
 
 1;
-
-=encoding utf8
-
-=head1 NAME
-
-Dist::Zilla::Plugin::TestMLIncluder - Ship your TestML version
-
-=head1 SYNOPSIS
-
-In dist.ini:
-
-   [TestMLIncluder]
-
-=head1 DESCRIPTION
-
-This module includes the version of TestML on your system with your module
-dist.
-
-=head1 AUTHOR
-
-Ingy döt Net <ingy@cpan.org>
-
-=head1 COPYRIGHT
-
-Copyright (c) 2014. Ingy döt Net.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-See http://www.perl.com/perl/misc/Artistic.html
-
-=cut
